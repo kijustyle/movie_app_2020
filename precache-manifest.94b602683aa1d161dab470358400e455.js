e07bd725f49651411f7fdbda2f282054def08655
kiju kim <kijukim@kijuui-MacBookPro.local>
2020-07-27 16:27:47 +0900

    Updates

---
+++ precache-manifest.94b602683aa1d161dab470358400e455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33440945fb0a6bda3742dec1bfc08aca",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9240a7f4c83e1d5d7ed0",
+    "url": "/movie_app_2020/static/css/main.3579b5d5.chunk.css"
+  },
+  {
+    "revision": "5b635e2e420b910582ea",
+    "url": "/movie_app_2020/static/js/2.6a4a412e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.6a4a412e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9240a7f4c83e1d5d7ed0",
+    "url": "/movie_app_2020/static/js/main.fb084383.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
